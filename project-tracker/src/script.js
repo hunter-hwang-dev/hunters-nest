30e9c81e1736a08d01853c5a7ebf8516ec4b2bfa
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-15 08:44:46 +0900

    chore: 폴더 여닫기 실행 오류, import ~ tree 주석 처리

--- project-tracker/src/script.js
+++ project-tracker/src/script.js
@@ -1,4 +1,4 @@
-import { Tree } from "./tree";
+// import { Tree } from "./tree";
 
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("folder")) {
@@ -9,21 +9,23 @@ document.addEventListener("click", function (event) {
   }
 });
 
-const tree = new Tree(1, "ROOT");
+// const tree = new Tree(1, "ROOT");
 
-tree.insert(1, 11, "parent 1");
-tree.insert(1, 12, "parent 2");
-tree.insert(1, 13, "parent 3");
+// tree.insert(1, 11, "parent 1");
+// tree.insert(1, 12, "parent 2");
+// tree.insert(1, 13, "parent 3");
 
-tree.insert(1, 111, "child 1");
-tree.insert(1, 112, "child 2");
-tree.insert(1, 121, "child 1");
-tree.insert(1, 122, "child 2");
-tree.insert(1, 123, "child 3");
-tree.insert(1, 131, "child 1");
+// tree.insert(1, 111, "child 1");
+// tree.insert(1, 112, "child 2");
+// tree.insert(1, 121, "child 1");
+// tree.insert(1, 122, "child 2");
+// tree.insert(1, 123, "child 3");
+// tree.insert(1, 131, "child 1");
 
-tree.insert(1, 1221, "grandchild 1");
-tree.insert(1, 1222, "grandchild 2");
-tree.insert(1, 1223, "grandchild 3");
+// tree.insert(1, 1221, "grandchild 1");
+// tree.insert(1, 1222, "grandchild 2");
+// tree.insert(1, 1223, "grandchild 3");
 
-tree.insert(1, 12221, "grand-grandchild 1");
+// tree.insert(1, 12221, "grand-grandchild 1");
+
+// console.log([...tree.preOrderTraversal()].map((x) => x.value));
