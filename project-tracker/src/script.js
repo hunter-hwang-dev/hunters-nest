9fe2adb41b31bd6caf2a1300dcab4a5f76c8da5c
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-15 09:57:41 +0900

    feat: js로 folder class 앞에 img 끼워넣기

--- project-tracker/src/script.js
+++ project-tracker/src/script.js
@@ -1,5 +1,16 @@
 import { Tree } from "./tree.js";
 
+const folders = document.getElementsByClassName("folder");
+const folderIcon = document.createElement("img");
+folderIcon.src = "assets/icons/folder-closed.svg";
+folderIcon.style.width = "18px";
+folderIcon.style.height = "18px";
+
+for (const folder of folders) {
+  //참고: getElementsByClassName은 HTMLCollection을 반환하기 때문에, forEach 메서드를 직접 사용할 수 없음. Array-like가 아니라 찐 Array여야.
+  folder.prepend(folderIcon.cloneNode()); // cloneNode()로 각 폴더에 독립적인 이미지 생성
+} //단, 이렇게 코드를 짜면 갱신할 때마다 깜빡거림. prepend로 끼워넣는데 시간 걸려서. 즉, 초기 세팅은 정적으로 stylesheet에서 처리해야 함.
+
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("folder")) {
     const subtree = event.target.nextElementSibling;
@@ -8,6 +19,11 @@ document.addEventListener("click", function (event) {
     }
   }
 });
+
+// .folder 요소의 첫 번째 자식으로 newElement 추가
+folder.prepend(newElement);
+
+//folder 클래스 맨 앞에 들어간 child 찾기
 
 const tree = new Tree(1, "ROOT");
 
