92aab4e4776e4343034a77d6b236a1220fcb7c6f
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-15 10:14:22 +0900

    refactor: 당장 사용하지 않는 부분 주석 처리

--- project-tracker/src/script.js
+++ project-tracker/src/script.js
@@ -1,15 +1,15 @@
 import { Tree } from "./tree.js";
 
-const folders = document.getElementsByClassName("folder");
-const folderIcon = document.createElement("img");
-folderIcon.src = "assets/icons/folder-closed.svg";
-folderIcon.style.width = "18px";
-folderIcon.style.height = "18px";
+// const folders = document.getElementsByClassName("folder");
+// const folderIcon = document.createElement("img");
+// folderIcon.src = "assets/icons/folder-closed.svg";
+// folderIcon.style.width = "18px";
+// folderIcon.style.height = "18px";
 
-for (const folder of folders) {
-  //참고: getElementsByClassName은 HTMLCollection을 반환하기 때문에, forEach 메서드를 직접 사용할 수 없음. Array-like가 아니라 찐 Array여야.
-  folder.prepend(folderIcon.cloneNode()); // cloneNode()로 각 폴더에 독립적인 이미지 생성
-} //단, 이렇게 코드를 짜면 갱신할 때마다 깜빡거림. prepend로 끼워넣는데 시간 걸려서. 즉, 초기 세팅은 정적으로 stylesheet에서 처리해야 함.
+// for (const folder of folders) {
+//   //참고: getElementsByClassName은 HTMLCollection을 반환하기 때문에, forEach 메서드를 직접 사용할 수 없음. Array-like가 아니라 찐 Array여야.
+//   folder.prepend(folderIcon.cloneNode()); // cloneNode()로 각 폴더에 독립적인 이미지 생성
+// } //단, 이렇게 코드를 짜면 갱신할 때마다 깜빡거림. prepend로 끼워넣는데 시간 걸려서. 즉, 초기 세팅은 정적으로 stylesheet에서 처리해야 함.
 
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("folder")) {
