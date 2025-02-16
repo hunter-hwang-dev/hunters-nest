e38c3685c74ad1afb683a72fce57baf0e2fb95b6
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-17 04:26:34 +0900

    bugfix: return이 for 루프 안에 있었네. 헉

--- cs-study/data-structure-algorithm/typeScript/sort/selectionSort.ts
+++ cs-study/data-structure-algorithm/typeScript/sort/selectionSort.ts
@@ -4,12 +4,18 @@ function selectionSort(numArr: number[]): number[] {
   let arr = [...numArr];
   let 현재최소 = { index: 0, value: arr[0] };
 
+  console.log(현재최소);
+
   for (let j = 0; j < arr.length; j++) {
     if (arr[j] < 현재최소.value) {
       현재최소.index = j;
       현재최소.value = arr[j];
+      console.log(현재최소);
     }
-    
+
+    arr[현재최소.index] = arr[0];
+    arr[0] = 현재최소.value;
+  }
   return arr;
 }
 
