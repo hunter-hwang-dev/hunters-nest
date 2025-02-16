4dc915b247b42e7f624ba4872bc3788fd35039e4
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-17 07:34:14 +0900

    refactor: 불필요한 반복 제외 위해 조건 break

--- cs-study/dsa/typeScript/sort/bubbleSort.ts
+++ cs-study/dsa/typeScript/sort/bubbleSort.ts
@@ -3,11 +3,16 @@ import { testArr1, testArr2 } from "../data";
 function bubbleSort(numArr: number[]): number[] {
   let arr = [...numArr];
   for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; //구조 분해 할당
+        swapped = true;
       }
     }
+
+    if (!swapped) break;
   }
 
   return arr;
