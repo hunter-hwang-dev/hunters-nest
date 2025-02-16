9bcad6c4438e00edc714a75e919c02cadbfc618a
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-17 04:35:20 +0900

    selection sort: 완성

--- cs-study/data-structure-algorithm/typeScript/sort/selectionSort.ts
+++ cs-study/data-structure-algorithm/typeScript/sort/selectionSort.ts
@@ -2,21 +2,22 @@ import { testArr } from "../data";
 
 function selectionSort(numArr: number[]): number[] {
   let arr = [...numArr];
-  let 현재최소 = { index: 0, value: arr[0] };
+  let 현재최소: { index: number; value: number };
 
-  console.log(현재최소);
-
-  for (let j = 0; j < arr.length; j++) {
-    if (arr[j] < 현재최소.value) {
-      현재최소.index = j;
-      현재최소.value = arr[j];
-      console.log(현재최소);
+  for (let i = 0; i < arr.length - 1; i++) {
+    현재최소 = { index: i, value: arr[i] };
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < 현재최소.value) {
+        현재최소.index = j;
+        현재최소.value = arr[j];
+      }
     }
+    arr[현재최소.index] = arr[i];
+    arr[i] = 현재최소.value;
+    console.log(arr);
   }
 
-  arr[현재최소.index] = arr[0];
-  arr[0] = 현재최소.value;
   return arr;
 }
 
-console.log(selectionSort(testArr));
+selectionSort(testArr);
