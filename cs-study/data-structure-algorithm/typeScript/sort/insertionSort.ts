aae9a5732945d02631f0d505f95ebd9249735670
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-17 05:04:02 +0900

    insert sort: 작은 루프 완성

--- cs-study/data-structure-algorithm/typeScript/sort/insertionSort.ts
+++ cs-study/data-structure-algorithm/typeScript/sort/insertionSort.ts
@@ -6,5 +6,20 @@ function insertionSort(numArr: number[]): number[] {
   return arr;
 }
 
-console.log(insertionSort(testArr1));
-console.log(insertionSort(testArr2));
+// console.log(insertionSort(testArr1));
+// console.log(insertionSort(testArr2));
+
+function insertion(baseArr: number[], insert: number): number[] {
+  let arr = [...baseArr];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] > insert) {
+      console.log(`${arr[i]} 앞에 ${insert}`);
+      arr.splice(i, 0, insert);
+    }
+  }
+
+  return arr;
+}
+
+console.log(insertion([1, 3, 5], 4));
