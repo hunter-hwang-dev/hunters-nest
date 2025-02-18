9acbefa77a4302f08b7232c0171a0f76bac24693
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-18 18:57:42 +0900

    insertion sort: 작은 루프 다시 작성

--- cs-study/dsa/typeScript/sort/insertionSort.ts
+++ cs-study/dsa/typeScript/sort/insertionSort.ts
@@ -1,35 +1,18 @@
 import { testArr1, testArr2 } from "../data";
 
-function insertionSort(numArr: number[]): number[] {
-  let arr = [...numArr];
+function insertionSort(numArr: number[], insert: number): number[] {
+  let arr = numArr;
 
-  for (let i = 1; i < arr.length; i++) {
-    //비교할 마지막 대상: arr[i]
-    for (let j = 0; j < i; j++) {
-      if (arr[j] > arr[i]) {
-        //arr[0] ~ arr[i-1] 중에서 작은 거 발견하면 이동
-        [arr[j], arr[i]] = [arr[i], arr[j]];
-        break; //그런데 재귀적으로, arr[0] ~ arr[i-1]은 이미 asc sort되어 있을 것이니 스왑하자마자 작은 루프 나가기
-      }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (insert > arr[i]) {
+      console.log(`insert > ${arr[i]}`);
+      arr.splice(i + 1, 0, insert);
+      break;
     }
   }
+
   console.log(arr);
   return arr;
 }
 
-insertionSort(testArr1);
-
-// function insertion(baseArr: number[], insert: number): number[] {
-//   let arr = [...baseArr];
-//   for (let j = arr.length - 1; j >= 0; j--) {
-//     if (arr[j] > insert) {
-//       console.log(`${arr[j]} 앞에 ${insert}`);
-//       arr.splice(j, 0, insert);
-//       break;
-//     }
-//   }
-
-//   return arr;
-// }
-
-// console.log(insertion([1, 3, 5], 4));
+insertionSort([1, 2, 3, 4, 5, 6, 7], 4);
