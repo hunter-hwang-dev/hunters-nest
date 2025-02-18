33600b918724f8fe21dea0464f292e256363c598
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-18 19:47:16 +0900

    insertion sort: 작은 루프 제일 간단화

--- cs-study/dsa/typeScript/sort/insertionSort.ts
+++ cs-study/dsa/typeScript/sort/insertionSort.ts
@@ -5,15 +5,13 @@ function insertionSort(numArr: number[]) {
   let insert = numArr[numArr.length - 1]; //4
 
   for (let i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] >= insert) {
-      console.log(`${arr[i]} >= insert`);
-    } else if (arr[i] < insert) {
-      console.log(`${i} 인덱스에서 ${arr[i]} < insert`);
+    if (arr[i] < insert) {
       arr.splice(i + 1, 0, insert);
-      console.log(arr, insert);
       break;
     }
   }
+
+  return arr;
 }
 
 insertionSort([1, 2, 3, 4, 5, 6, 7, 4]);
