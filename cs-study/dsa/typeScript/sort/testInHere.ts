58deb7e0e15ae3805573d6a32c68e44560c82523
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-19 02:00:53 +0900

    refactor: sort 테스트 코드 한곳에 모음

--- cs-study/dsa/typeScript/sort/testInHere.ts
+++ cs-study/dsa/typeScript/sort/testInHere.ts
@@ -4,10 +4,15 @@ import { insertionSort } from "./insertionSort";
 //import { quickSort } from "./quickSort";
 import { radixSort } from "./radixSort";
 
-import { testArr1, testArr2, testArr3 } from "../data";
+const testArr1: number[] = [7, 9, 11, 3, 12];
+const testArr2: number[] = [64, 34, 25, 12, 22, 11, 90, 5];
+const testArr3: number[] = [170, 45, 75, 90, 802, 24, 2, 66];
 
 console.log(bubbleSort(testArr1));
 console.log(selectionSort(testArr1));
 console.log(insertionSort(testArr2));
 //console.log(quickSort(testArr2));
-console.log(radixSort(testArr3));
+console.log(radixSort(testArr3));\
+
+//typescript는 변수 선언 시 기본이 "전체 적용"이므로 여기에다가 테스트용 array를 몰아두겠다.
+//ts-node sort.ts 이런 명령어로 터미널에서 콘솔 실행 가능 (npm install -g ts-node typescript 설치 필요)
