26e2ca39dafeed214f1a26b273963fbb5807eecd
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-19 04:35:53 +0900

    refactor: class 헷갈리니 key-value object로 1단계 child부터 하드코딩

--- project-tracker/src/utils/tree.js
+++ project-tracker/src/utils/tree.js
@@ -1,9 +1,26 @@
 //n-ary tree: 첫째를 대장으로 내세우면 이진 트리로 표현 가능!
 
-class TreeNode {
-  constructor(data) {
-    this.data = data;
-    this.parent = null;
-    this.child = [];
-  }
-}
+let rootNode = { data: "ROOT", parent: null, child: [] };
+
+let node00 = {
+  data: "parent 1",
+  parent: rootNode,
+  child: [],
+};
+rootNode.child.push(node00);
+
+let node01 = {
+  data: "parent 2",
+  parent: rootNode,
+  child: [],
+};
+rootNode.child.push(node01);
+
+let node02 = {
+  data: "parent 2",
+  parent: rootNode,
+  child: [],
+};
+rootNode.child.push(node02);
+
+console.log(rootNode);
