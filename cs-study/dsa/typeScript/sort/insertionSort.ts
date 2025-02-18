9a174b518cd10945a48df5d93e63c36f953e9919
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-18 19:46:52 +0900

    insertion sort: 작은 루프 다시 정돈

--- cs-study/dsa/typeScript/sort/insertionSort.ts
+++ cs-study/dsa/typeScript/sort/insertionSort.ts
@@ -1,64 +1,19 @@
 import { testArr1, testArr2 } from "../data";
 
-function insertion(numArr: number[], insert: number): number[] {
-  let arr = numArr;
+function insertionSort(numArr: number[]) {
+  let arr = numArr.slice(0, -1); //[1, 2, 3, 4, 5, 6, 7]
+  let insert = numArr[numArr.length - 1]; //4
 
   for (let i = arr.length - 1; i >= 0; i--) {
-    if (insert > arr[i]) {
-      console.log(`insert > ${arr[i]}`);
+    if (arr[i] >= insert) {
+      console.log(`${arr[i]} >= insert`);
+    } else if (arr[i] < insert) {
+      console.log(`${i} 인덱스에서 ${arr[i]} < insert`);
       arr.splice(i + 1, 0, insert);
+      console.log(arr, insert);
       break;
     }
   }
-
-  console.log(arr);
-  return arr;
 }
 
-//insertion([1, 2, 3, 4, 5, 6, 7], 4);
-
-function insertionSort(numArr: number[]): number[] {
-  let length = numArr.length;
-  let arr: number[] = [];
-
-  if (length == 0) {
-    console.log(arr);
-  } else if (length == 1) {
-    arr.push(numArr[0]);
-    console.log(arr);
-  } else if (length == 2) {
-    if (numArr[0] > numArr[1]) {
-      arr.push(numArr[1]);
-      arr.push(numArr[0]);
-    } else {
-      arr.push(numArr[0]);
-      arr.push(numArr[1]);
-    }
-    console.log(arr);
-  } else if (length == 3) {
-    let insert = numArr[2];
-
-    if (numArr[0] > numArr[1]) {
-      arr.push(numArr[1]);
-      arr.push(numArr[0]);
-    } else {
-      arr.push(numArr[0]);
-      arr.push(numArr[1]);
-    }
-
-    for (let i = arr.length - 1; i >= 0; i--) {
-      if (insert > arr[i]) {
-        console.log(`insert > ${arr[i]}`);
-        arr.splice(i + 1, 0, insert);
-        break;
-      }
-    }
-    console.log(arr);
-  }
-  return arr;
-}
-
-insertionSort([1]);
-insertionSort([1, 3]);
-insertionSort([3, 1]);
-insertionSort([1, 3, 2]);
+insertionSort([1, 2, 3, 4, 5, 6, 7, 4]);
