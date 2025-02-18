e662ee3f763a8c835ae063cbe6beb9b6cf80c432
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-18 20:49:24 +0900

    insertion sort: 큰 루프까지 완료! 함수 이름을 그대로 쓰지 않으면 안되는 것 주의

--- cs-study/dsa/typeScript/sort/insertionSort.ts
+++ cs-study/dsa/typeScript/sort/insertionSort.ts
@@ -1,59 +1,37 @@
-// import { testArr1, testArr2 } from "../data";
-
-// // [1, 2, 3, 4, 5, 6, 7], 4
-// function insert(numArr: number[], insert: number): number[] {
-//   let arr = [...numArr];
-//   let inserted = false;
-
-//   for (let i = arr.length; i >= 0; i--) {
-//     if (arr[i] <= insert) {
-//       arr.splice(i + 1, 0, insert);
-//       inserted = true;
-//       break;
-//     }
-//   }
-
-//   if (!inserted) {
-//     //arr[i]가 전부 insert보다 큰 경우
-//     arr.splice(0, 0, insert);
-//   }
-
-//   console.log(arr);
-//   return arr;
-// }
-
-// insert([1, 2, 3, 4, 5, 6, 7], 4);
-// insert([1, 2, 3, 4, 5, 6], 0);
-// insert([1, 2, 3, 4, 5, 6], 12);
-
-// function insertionSortEmpty(numArr: []): number[] {
-//   //빈 배열인 경우
-//   return [];
-// }
-
-// function insertionSort0(numArr: [number]): number[] {
-//   //항이 1개짜리, 즉 최대 인덱스가 0인 배열인 경우
-//   return numArr;
-// }
+import { testArr1, testArr2 } from "../data";
 
 function insertionSort(numArr: number[]) {
-  let [slice, insert] = [numArr.slice(0, -1).sort(), ...numArr.slice(-1)];
-  console.log(slice, insert);
+  if (numArr.length == 0) {
+    return [];
+  } else if (numArr.length == 1) {
+    return [numArr[0]];
+  } else {
+    let [slice, insert] = [
+      insertionSort(numArr.slice(0, -1)),
+      ...numArr.slice(-1),
+    ];
 
-  let inserted = false;
-  for (let i = slice.length - 1; i >= 0; i--) {
-    if (slice[i] > insert) {
-    } else {
-      slice.splice(i + 1, 0, insert);
-      inserted = true;
-      break;
+    let inserted = false;
+    for (let i = slice.length - 1; i >= 0; i--) {
+      if (slice[i] > insert) {
+      } else {
+        slice.splice(i + 1, 0, insert);
+        inserted = true;
+        break;
+      }
     }
-  }
-  if (!inserted) {
-    slice.splice(0, 0, insert);
-  }
+    if (!inserted) {
+      slice.splice(0, 0, insert);
+    }
 
-  console.log(slice);
+    return slice;
+  }
 }
 
-insertionSort([3, 5, 2, 7]);
+console.log(insertionSort([]));
+console.log(insertionSort([3]));
+console.log(insertionSort([3, 5]));
+console.log(insertionSort([3, 5, 6]));
+console.log(insertionSort([3, 5, 6, 4]));
+console.log(insertionSort(testArr1));
+console.log(insertionSort(testArr2));
