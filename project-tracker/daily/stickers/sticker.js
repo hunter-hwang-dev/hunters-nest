463df586f2f6cebde9aca5be5ad159d4acb06373
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-20 22:23:44 +0900

    feat: 새로 추가하는 스티커 내용 바꿀 때에도 적용 위해 body 에 통째로 이벤트 리스너 달기

--- project-tracker/daily/stickers/sticker.js
+++ project-tracker/daily/stickers/sticker.js
@@ -5,23 +5,27 @@ let stickers = [
 ]; //기존에 있던 스티커들 id
 let offsetX = 0;
 let offsetY = 0;
-
-document.querySelectorAll(".sticker-memo").forEach((memo) => {
-  memo.addEventListener("blur", (e) => {
-    //요소가 포커스 잃을 때 발생하는 "blur" 이벤트
-    const container = e.target.closest(".sticker-container"); // 상위 컨테이너 찾기
-    //e.target.closest는 부모 > 조부모 > 조조부모... 이렇게 node 타다가 해당하는 것 있으면 멈추고 반환.
-
-    if (container) {
-      const id = parseInt(container.id); // id 추출
-      const sticker = stickers.find((s) => s.id === id); // 해당 sticker 찾기
-      if (sticker) {
-        sticker.text = e.target.innerText; // 내용 업데이트
-        console.log(`Sticker ${id} updated:`, sticker.text); // 디버깅 로그
-      }
+document.body.addEventListener(
+  "blur",
+  (e) => {
+    if (e.target.classList.contains("sticker-memo")) {
+      refreshSticker(e.target);
     }
-  });
-});
+  },
+  true
+);
+
+function refreshSticker(target) {
+  const container = target.closest(".sticker-container"); // 상위 컨테이너 찾기
+  if (container) {
+    const id = parseInt(container.id); // id 추출
+    const sticker = stickers.find((s) => s.id === id); // 해당 sticker 찾기
+    if (sticker) {
+      sticker.text = target.innerText; // 내용 업데이트
+      console.log(`Sticker ${id} updated:`, sticker.text); // 디버깅 로그
+    }
+  }
+}
 
 function drag(event) {
   offsetX = event.offsetX;
