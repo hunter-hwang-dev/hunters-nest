47edee954cf126cb04b1e8c23d9e2fcde99c7a08
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-27 13:55:47 +0900

    refactor: saveBtn initSketch에 포함

--- project-tracker/daily/stickers/sticker.js
+++ project-tracker/daily/stickers/sticker.js
@@ -1,7 +1,6 @@
 import { dummySketch } from "./dummy.js"; //vanilla js에서 확장자명 주의!
 import { appendStickerOnBody } from "./stickerpack.js";
 
-const saveBtn = document.getElementById("save");
 let draft = [];
 
 initSketch(dummySketch);
@@ -9,16 +8,17 @@ syncDraftText();
 syncDraftPosition();
 
 function initSketch() {
+  const saveBtn = document.getElementById("save");
   const savedData = JSON.parse(localStorage.getItem("sketch"));
   savedData.forEach((sticker) => {
     appendStickerOnBody(sticker);
     draft.push(sticker);
   });
+  saveBtn.addEventListener("click", (e) => {
+    localStorage.setItem("sketch", JSON.stringify(draft));
+    console.log(draft);
+  });
 }
-
-saveBtn.addEventListener("click", (e) => {
-  localStorage.setItem("sketch", JSON.stringify(draft));
-});
 
 function syncDraftText() {
   document.body.addEventListener("focusout", (e) => {
