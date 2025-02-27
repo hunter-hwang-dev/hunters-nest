4046195762245fa4330b482fe980eeb60cbe09ac
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-27 13:54:02 +0900

    feat: 드래그 시 id, 드롭 시 마우스 좌표 출력

--- project-tracker/daily/stickers/sticker.js
+++ project-tracker/daily/stickers/sticker.js
@@ -5,6 +5,8 @@ const saveBtn = document.getElementById("save");
 let draft = [];
 
 initSketch(dummySketch);
+syncDraftText();
+syncDraftPosition();
 
 function initSketch() {
   const savedData = JSON.parse(localStorage.getItem("sketch"));
@@ -18,11 +20,30 @@ saveBtn.addEventListener("click", (e) => {
   localStorage.setItem("sketch", JSON.stringify(draft));
 });
 
-document.body.addEventListener("focusout", (e) => {
-  if (e.target.classList.contains("sticker-memo")) {
-    const id = e.target.parentElement.parentElement.id;
-    const text = e.target.innerText;
-    draft[id].text = text;
-    console.log(draft);
-  }
-});
+function syncDraftText() {
+  document.body.addEventListener("focusout", (e) => {
+    if (e.target.classList.contains("sticker-memo")) {
+      const id = e.target.parentElement.parentElement.id;
+      const text = e.target.innerText;
+      draft[id].text = text;
+      console.log(draft);
+    }
+  });
+}
+
+function syncDraftPosition() {
+  document.addEventListener("dragstart", (e) => {
+    console.log(e.target.id);
+  });
+
+  document.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  document.addEventListener("drop", (e) => {
+    e.preventDefault();
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+
+    console.log("dropped at:", mouseX, mouseY);
+  });
+}
