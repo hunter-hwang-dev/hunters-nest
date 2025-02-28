3f0d1d789f530dbfacb104c8d906fccbefbe3a3b
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-28 20:04:50 +0900

    feat: 글 작성 페이지와 연결 - frontend.js 반영 안되는 문제

--- vanilla-js/flowbite/scripts/backend.js
+++ vanilla-js/flowbite/scripts/backend.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path = require("path"); // path 모듈 불러오기
 const app = express();
 
 app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
+  console.log("http://localhost:8080");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html")); //서버 속 경로랑 헷갈리지 않게 절대경로로 작성
 });
