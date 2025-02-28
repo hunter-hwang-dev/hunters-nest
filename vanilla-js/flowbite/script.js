523f574d286bf89049c862a41b1e15906e186dda
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-28 18:47:02 +0900

    feat: 에디터 내 텍스트 html / json 출력 테스트 코드

--- vanilla-js/flowbite/script.js
+++ vanilla-js/flowbite/script.js
@@ -2,13 +2,13 @@ import { Editor } from "https://esm.sh/@tiptap/core@2.6.6";
 import StarterKit from "https://esm.sh/@tiptap/starter-kit@2.6.6";
 
 window.addEventListener("load", function () {
-  if (document.getElementById("wysiwyg-typography-example")) {
+  if (document.getElementById("wysiwyg-export-example")) {
     // tip tap editor setup
     const editor = new Editor({
-      element: document.querySelector("#wysiwyg-typography-example"),
+      element: document.querySelector("#wysiwyg-export-example"),
       extensions: [StarterKit],
       content:
-        '<p>Flowbite is an <strong>open-source library of UI components</strong> based on the utility-first Tailwind CSS framework featuring dark mode support, a Figma design system, and more.</p><p>It includes all of the commonly used components that a website requires, such as buttons, dropdowns, navigation bars, modals, datepickers, advanced charts and the list goes on.</p><ul><li>Over 600+ open-source UI components</li><li>Supports dark mode and RTL</li><li>Available in React, Vue, Svelte frameworks</li></ul><p>Here is an example of a button component:</p><pre><code>&#x3C;button type=&#x22;button&#x22; class=&#x22;text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800&#x22;&#x3E;Default&#x3C;/button&#x3E;</code></pre><p>Learn more about all components from the <a href="https://flowbite.com/docs/getting-started/introduction/">Flowbite Docs</a>.</p>',
+        '<p>Flowbite is an <strong>open-source library of UI components</strong> based on the utility-first Tailwind CSS framework featuring dark mode support, a Figma design system, and more.</p><p>It includes all of the commonly used components that a website requires, such as buttons, dropdowns, navigation bars, modals, datepickers, advanced charts and the list goes on.</p><p>Here is an example of a button component:</p><code>&#x3C;button type=&#x22;button&#x22; class=&#x22;text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800&#x22;&#x3E;Default&#x3C;/button&#x3E;</code><p>Learn more about all components from the <a href="https://flowbite.com/docs/getting-started/introduction/">Flowbite Docs</a>.</p>',
       editorProps: {
         attributes: {
           class:
@@ -17,54 +17,34 @@ window.addEventListener("load", function () {
       },
     });
 
+    const sourceCodeModal = FlowbiteInstances.getInstance(
+      "Modal",
+      "source-code-modal"
+    );
+    const sourceCodeWrapper = document.getElementById("sourceCode");
+
     // set up custom event listeners for the buttons
     document
-      .getElementById("toggleListButton")
+      .getElementById("toggleHTMLButton")
       .addEventListener("click", () => {
-        editor.chain().focus().toggleBulletList().run();
-      });
-    document
-      .getElementById("toggleOrderedListButton")
-      .addEventListener("click", () => {
-        editor.chain().focus().toggleOrderedList().run();
-      });
-    document
-      .getElementById("toggleBlockquoteButton")
-      .addEventListener("click", () => {
-        editor.chain().focus().toggleBlockquote().run();
-      });
-    document.getElementById("toggleHRButton").addEventListener("click", () => {
-      editor.chain().focus().setHorizontalRule().run();
-    });
-    document
-      .getElementById("toggleCodeBlockButton")
-      .addEventListener("click", () => {
-        editor.chain().focus().toggleCodeBlock().run();
+        // basically just use editor.getHTML(); to get the raw html
+
+        sourceCodeWrapper.innerHTML = editor.getHTML().replace(/&/g, "&amp;"); // Escape & character
+        // .replace(/</g, "&lt;") // Escape < character
+        // .replace(/>/g, "&gt;") // Escape > character
+        // .replace(/"/g, "&quot;") // Escape " character
+        // .replace(/'/g, "&#039;"); // Escape ' character
       });
 
-    // typography dropdown
-    const typographyDropdown = FlowbiteInstances.getInstance(
-      "Dropdown",
-      "typographyDropdown"
-    );
-
     document
-      .getElementById("toggleParagraphButton")
+      .getElementById("toggleJSONButton")
       .addEventListener("click", () => {
-        editor.chain().focus().setParagraph().run();
-        typographyDropdown.hide();
-      });
+        // basically just use editor.getJSON(); to get the raw json
 
-    document.querySelectorAll("[data-heading-level]").forEach((button) => {
-      button.addEventListener("click", () => {
-        const level = button.getAttribute("data-heading-level");
-        editor
-          .chain()
-          .focus()
-          .toggleHeading({ level: parseInt(level) })
-          .run();
-        typographyDropdown.hide();
+        sourceCode.innerHTML = JSON.stringify(editor.getJSON(), null, 2)
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;");
       });
-    });
   }
 });
