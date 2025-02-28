ff40a176868bc4574b0931b7be79413812142155
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2025-02-28 21:03:59 +0900

    feat: 환경변수 경로 수정

--- vanilla-js/flowbite/server.js
+++ vanilla-js/flowbite/server.js
@@ -1,8 +1,23 @@
+require("dotenv").config();
+const url = process.env.MONGO_DB_URI;
 const express = require("express");
 const path = require("path"); // path 모듈 불러오기
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public"))); // public 폴더가 정적 파일로 제공됨
+
+const { MongoClient } = require("mongodb");
+
+let db;
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // 서버 실행
 app.listen(8080, () => {
